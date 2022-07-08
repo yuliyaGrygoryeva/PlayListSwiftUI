8ffc260a2e26e66eab46b986551d58913b868511
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-07-08 13:00:04 -0700

    Initial commit

---
+++ PlayList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlayList
+//
+//  Created by Yuliya  on 7/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PlayList/PlayListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayListApp.swift
+//  PlayList
+//
+//  Created by Yuliya  on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct PlayListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
